091667ecd8d174368ff335484a5480654aa164fa
mas-th <thieunv1211996@gmail.com>
2025-07-09 12:11:43 +0700

    fix error index

--- src/main/java/com/kham_pha_web/api/userapi/UserApiController.java
+++ src/main/java/com/kham_pha_web/api/userapi/UserApiController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserApiController {
     @GetMapping
     public String Users() {
-        return "users/index";
+        return "users";
     }
 }
